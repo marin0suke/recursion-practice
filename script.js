be946538b3d59dae3290eea35b9d158459e13c3b
marin0suke <monk.marina@gmail.com>
2024-11-15 15:25:35 +1000

    Recursive version of Fibonacci

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fibs(num) {
 	let fibNumbers = [];
 	
-	for (let i = 0; i < num; i++) {
+	for (let i = 0; i <= num; i++) {
 		if (i === 0 || i === 1) {
 			fibNumbers[i] = i; 
 		} else {
@@ -9,5 +9,14 @@ function fibs(num) {
 		}
 	} 
 	return fibNumbers;
+}
+
+
+function fibsRec(num) {
+    if (num === 0 || num === 1) return num;
+
+    // return a single fib number.
+    //to get there, we need to calc each [i] of fibNumbers.
+    return (fibsRec(num - 1) + fibsRec(num - 2));
 }
 
